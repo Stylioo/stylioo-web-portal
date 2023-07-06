bf9123155a89c23f498b054abe3628ccfb83477c
Janith Madarasinghe <janithpm9991@gmail.com>
2023-07-06 15:27:08 +0530

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/home"
+import AppointmentPage from "./pages/appoinments"
+import Sidebar from "./components/sidebar"
+import Appbar from "./components/appbar"
 
+function App() {
   return (
-    <>
-    <h1>Web Portal</h1>
-    </>
+    <BrowserRouter>
+      <Appbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/appointment" element={<AppointmentPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
