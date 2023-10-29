b2c20aa9b4471632132a6cb2e483bdebd87e35b8
Janith Madarasinghe <73998693+Janithpm@users.noreply.github.com>
2023-10-29 23:10:39 +0530

    beauticians

--- src/pages/allbeautician/HomePage.tsx
+++ src/pages/allbeautician/HomePage.tsx
@@ -1,87 +1,88 @@
-// import React from 'react'
-
-// export const AllBeauticianPage = () => {
-//   return (
-//     <div>AllBeauticianPage</div>
-//   )
-// }
-
-// export default AllBeauticianPage
-
-import * as React from 'react';
+import axios from '@/axios';
+import Loading from '@/components/Loading';
+import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
 
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
+function AllBeauticianPage() {
 
- function AllBeauticianPage() {
+  const [beautician, setBeautician] = useState<any[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getAllBeauticians = async () => {
+    try {
+      setIsLoading(true)
+      const response = await axios.get('/employee/role/beautician')
+      if (response.data.success) {
+        console.log(response.data.data);
+        setBeautician(response.data.data)
+      }
+    } catch (error) {
+      console.log(error)
+      setBeautician([])
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getAllBeauticians()
+  }, [])
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <main>
-        {/* Hero unit */}
-      
-        <Container sx={{ py: 8 }} maxWidth='lg'>
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={4} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                      height:'350px',
-                    }}
-                    image="https://source.unsplash.com/random?people"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Chirasi Amaya
-                    </Typography>
-                    <Typography>
-                      Talented hair cutting beautician with 5 years of experience in hair cutting and hair styling.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" variant="contained" color='success'>View More Details</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-    </ThemeProvider>
+    <>
+      {
+        isLoading ? <Loading /> :
+          <Box sx={{
+            mt: 1
+          }}>
+            <Typography
+              variant="h4"
+              sx={{
+                mb: 2
+              }}
+            >Beauticians</Typography>
+            <Grid container spacing={8} >
+
+              {beautician.map((beauti) => (
+                <Grid item key={beauti.id} xs={12} sm={4} md={3}>
+                  <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  >
+                    <CardMedia
+                      component="div"
+                      sx={{
+                        height: '200px',
+                      }}
+                      image={beauti.image ? `https://stylioo.blob.core.windows.net/images/${beauti.image}` : 'https://source.boringavatars.com/beam/120/Stefan?colors=264653,f4a261,e76f51'}
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {beauti.first_name} {beauti.last_name}
+                      </Typography>
+                      {/* <Typography>
+                        Talented hair cutting beautician with 5 years of experience in hair cutting and hair styling.
+                      </Typography> */}
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" variant="contained" color='success'>View More Details</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+      }
+    </>
   );
 }
 
